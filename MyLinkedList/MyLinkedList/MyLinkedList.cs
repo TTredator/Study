b7a75a0e6f3dcb2f4c7c0e8d364b7328aaeeb0f8
unknown <antonymoiseev@gmail.com>
2022-06-15 14:26:35 +0300

    This is my LinkedList

---
+++ MyLinkedList/MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLinkedList
+{
+    public class myLinkedList
+    {
+        public Node headNode;
+        public myLinkedList()
+        {
+            headNode = null;
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                headNode.AddToEnd(data);
+            }
+        }
+        public void AddToBeginning(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = headNode;
+                headNode = temp;
+            }
+        }
+        public void Print()
+        {
+
+            if (headNode.next != null)
+            {
+                headNode.Print();
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyLinkedList/MyLinkedList/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(data);
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else
+            {
+                next.AddToEnd(data);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
